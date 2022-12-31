6494a4a840464db05d00920ee778487c42b83b78
s1gmoyd <tomjhurrell@gmail.com>
2022-12-31 11:44:24 +0000

    refactor user constructor, declutter

--- src/classes/user.js
+++ src/classes/user.js
@@ -7,7 +7,7 @@ Reference: https://stackoverflow.com/a/27612338
 Published: 12/23/2014 - https://stackoverflow.com/users/4386702/halbgut
 Retrieved: 12/30/2022
 */
-function createRandomString (length, callback) {
+function createRandomString (callback, length) {
     var randomBase64String = '',
     checkReadyness;
   
@@ -29,49 +29,38 @@ export class UserTable {
     constructor() {
 
     }
-    getUserById(id, callback) {
-        var sql = `SELECT * FROM Users WHERE ID = ${db.escape(id)};`
-
-        db.query(sql, function(error, result, fields) {
-
-            if (error) {
-                return callback(null,`${error.sqlMessage}`);
-            }
-
-            new User(result[0].ID,
-            result[0].Name,
-            result[0].Email,
-            result[0].Password,
-            result[0].Discord,
-            result[0].Twitter,
-            result[0].TwoFA,
-            function(response) {
-                return callback(response, null)
-            });
-        });
+    getUserById(callback, id) {
+        new User(function (obj, err) {
+            callback(obj, err);
+        }, id)
     }
 }
 
 export class User {
-    constructor(id=null, name, email, password, discord=null, twitter=null, twofa=false, callback) {
+    constructor(callback, id=null, name=null, email=null, password=null, discord=null, twitter=null, twofa=false) {
         this.id; this.name; this.email; this.password; this.discord; this.twitter; this.twofa;
         
-        this.name=name;
-        this.email=email;
-
-        const myBitArray = sjcl.hash.sha256.hash(password);
-        const myHash = sjcl.codec.hex.fromBits(myBitArray);
-
-        this.password=myHash;
-        if(discord) { this.discord=discord }
-        if(twitter) { this.twitter=twitter }
         if (id) {
-            this.id=id
-            if (twofa) {
-                this.twofa=twofa
-            }
-            return callback(this, null);
+            var sql = `SELECT * FROM Users WHERE ID = ${db.escape(id)};`;
+
+            db.query(sql, function(error, result, fields) {
+
+                if (error) {
+                    return callback(null,`${error.sqlMessage}`);
+                }
+    
+                this.id=result[0].ID;
+                this.name=result[0].Name;
+                this.email=result[0].Email;
+                this.password=result[0].Password;
+                if (result[0].Discord) {this.discord=result[0].Discord}
+                if (result[0].Twitter) {this.twitter=result[0].Twitter}
+                if (result[0].TwoFA) {this.twofa=result[0].TwoFA}
+
+                return callback(this, null);
+            });
         }
+
         var sql = `INSERT INTO Participants () VALUES ();`
 
         db.query(sql, function(error, result, fields) {
@@ -79,11 +68,21 @@ export class User {
             if (error) {
                 return callback(null,`${error.sqlMessage}`);
             }
-                
+
+            this.name=name;
+            this.email=email;
+
+            const myBitArray = sjcl.hash.sha256.hash(password);
+            const myHash = sjcl.codec.hex.fromBits(myBitArray);
+
+            this.password=myHash;
+            if(discord) { this.discord=discord }
+            if(twitter) { this.twitter=twitter }
+
             this.id = result.insertId;
 
             if(twofa) {
-                createRandomString(15, function(response) {
+                createRandomString(function(response) {
                     this.twofa=response;
 
                     var sqlfields = `INSERT INTO Users (ID, Name, Email, Password`
@@ -106,40 +105,32 @@ export class User {
 
                         return callback(this, null);
                     });
-                });
+                }, 15);
             }
         });
     }
 
     toString() {
-        return `"${this.name}" (id: ${this.id})`
+        return `"${this.name}" (id: ${this.id})`;
     }
 
-    createTeam(name, twitter=null, callback) {
-        return callback(new Team(name, this, twitter, callback));
-    }
+    // addUserAsAdmin(user, event, callback) {
+    //     if (this.id === event.ownerid && user.id != this.id) {
 
-    createEvent(name, twitter=null, callback) {
-        return callback(new Event(name, this, org, callback));
-    }
+    //         var sql = `INSERT INTO EventAdmins (EventID, AdminID) VALUES (${db.escape(event.id)},${db.escape(user.id)})`
 
-    addUserAsAdmin(user, event, callback) {
-        if (this.id === event.ownerid && user.id != this.id) {
+    //         db.query(sql, function(error, result, fields) {
 
-            var sql = `INSERT INTO EventAdmins (EventID, AdminID) VALUES (${db.escape(event.id)},${db.escape(user.id)})`
+    //             if (error) {
+    //                 return callback(`${error.sqlMessage}`);
+    //             }
 
-            db.query(sql, function(error, result, fields) {
+    //             return callback(null);
 
-                if (error) {
-                    return callback(`${error.sqlMessage}`);
-                }
+    //         });
 
-                return callback(null);
-
-            });
-
-        } else {
-            callback(`${this.toString()} does not own event ${event.toString()}`)
-        }
-    }
+    //     } else {
+    //         callback(`${this.toString()} does not own event ${event.toString()}`)
+    //     }
+    // }
 }
